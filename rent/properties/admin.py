d817872bf3e5554e61fa8466874e49013204e11e
Lhakpa Doma Sherpa <lhakpa0366@xavier.edu.np>
2026-03-12 18:46:39 +0545

    new

--- rent/properties/admin.py
+++ rent/properties/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Property, PropertyImage
 
-# Register your models here.
+
+class PropertyImageInline(admin.TabularInline):
+    model = PropertyImage
+    extra = 2
+
+
+@admin.register(Property)
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = ['title', 'owner', 'category', 'price', 'location', 'status', 'created_at']
+    list_filter = ['category', 'status', 'furnishing', 'city']
+    search_fields = ['title', 'location', 'owner__username']
+    inlines = [PropertyImageInline]
+    readonly_fields = ['created_at', 'updated_at']
